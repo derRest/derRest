ec8fb2886a51b0784e6b60a9a7b8f31252411a58
Florian <it14013@lehre.dhbw-stuttgart.de>
2016-10-06 09:34:50 +0200

    added maze generation

--- index.php
+++ index.php
@@ -11,6 +11,36 @@ echo '<br>';
 echo '<br>';
 echo phpversion();
 
+require_once 'src/Maze.php';
+
+$x = Maze::DEFAULT_MAZE_WIDTH;
+$y = Maze::DEFAULT_MAZE_HEIGHT;
+
+if (!empty($_GET['x']) && is_numeric($_GET['x'])) {
+    $x = $_GET['x'];
+}
+if (!empty($_GET['y']) && is_numeric($_GET['y'])) {
+    $y = $_GET['y'];
+}
+$m = new Maze((int) $x, (int) $y);
+echo '<pre>';
+
+$start = getTime();
+$m->generate();
+$finish = getTime();
+$total_time = round(($finish - $start), 4);
+
+$m->display();
+echo '</pre>';
+echo '<p>This ' . $x . 'x' . $y . ' maze was generated in ' . $total_time . ' seconds.</p>';
+
+function getTime()
+{
+    $time = explode(' ', microtime());
+    $time = $time[1] + $time[0];
+    return $time;
+} //end getTime
+
 ?>
 <html>
 <head>
