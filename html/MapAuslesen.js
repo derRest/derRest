928a0e00e98a3ed45bee5b0ed50bb08f9a7cb164
Jan Jäger - PC2520 <privat@jan-jaeger.de>
2016-10-18 12:44:32 +0200

    CHANGED Map ausgabe

--- html/MapAuslesen.js
+++ html/MapAuslesen.js
@@ -7,13 +7,10 @@ var maze = {};
 maze.loadMap = function (map) {
 
     $.each(map, function (index, value) {
-      //  console.log(index + ": " + value );
-        console.log("value" + index + ": " + value );
+       console.log("value" + index + ": " + value );
 
       //  maze.printMap('[','map')
         for(var i = 0; i < value.length;i++){
-
-        //    console.log("for" + i + ": " + value[i]);
 
             if(value[i]==0){
                 maze.printMap("__",'map')
@@ -26,11 +23,9 @@ maze.loadMap = function (map) {
         //maze.printMap(']','map')
         maze.printMap('<br>','map')
     })
-
 }
 
 
 maze.printMap = function (zeichen, className) {
-
     jQuery("." + className).append(zeichen);
 }
