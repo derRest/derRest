ce24d2d855233a2477d32e909306f0ddeb69237c
Jan Jäger - PC2520 <privat@jan-jaeger.de>
2016-10-18 11:40:08 +0200

    ADD MapsAuslesen.js

--- html/MapAuslesen.js
+++ html/MapAuslesen.js
@@ -2,10 +2,13 @@
  * Created by admpc2520 on 18.10.2016.
  */
 
+var maze = {};
+
+maze.helloWorld = function (map) {
+    console.log(MapAuslesen(map));
+}
 
 function MapAuslesen (map) {
-
-
     return map;
 }
 
