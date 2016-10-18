6550fc2d0f9c6b07518f802d79b19ad876250e87
Jan Jäger - PC2520 <privat@jan-jaeger.de>
2016-10-18 12:36:10 +0200

    ADD output Map

--- html/MapAuslesen.js
+++ html/MapAuslesen.js
@@ -1,14 +1,36 @@
 /**
- * Created by admpc2520 on 18.10.2016.
+ * Created by JanJJ on 18.10.2016.
  */
 
 var maze = {};
 
-maze.helloWorld = function (map) {
-    console.log(MapAuslesen(map));
+maze.loadMap = function (map) {
+
+    $.each(map, function (index, value) {
+      //  console.log(index + ": " + value );
+        console.log("value" + index + ": " + value );
+
+      //  maze.printMap('[','map')
+        for(var i = 0; i < value.length;i++){
+
+        //    console.log("for" + i + ": " + value[i]);
+
+            if(value[i]==0){
+                maze.printMap("__",'map')
+            }else if(value[i]==1){
+                maze.printMap('##','map')
+            }else if(value[i]==2){
+                maze.printMap('&#127852;','map')
+            }
+        }
+        //maze.printMap(']','map')
+        maze.printMap('<br>','map')
+    })
+
 }
 
-function MapAuslesen (map) {
-    return map;
-}
 
+maze.printMap = function (zeichen, className) {
+
+    jQuery("." + className).append(zeichen);
+}
