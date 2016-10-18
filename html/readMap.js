0f8b6f8d5f17af5a6c627ffc92548b1bb2f6d44f
Jan Jäger - PC2520 <privat@jan-jaeger.de>
2016-10-18 14:38:06 +0200

    ADJUST readMap.js Ausgabe der Karte in <span> Struktur

--- html/readMap.js
+++ html/readMap.js
@@ -5,27 +5,44 @@
 var maze = {};
 
 maze.loadMap = function (map) {
+    var x=0;
 
     $.each(map, function (index, value) {
-       console.log("value" + index + ": " + value );
+      // console.log("value" + index + ": " + value );
+        var text = "";
+        x++;
 
-      //  maze.printMap('[','map')
+        text += maze.createStruct('','map',x,'start');
+
         for(var i = 0; i < value.length;i++){
-
             if(value[i]==0){
-                maze.printMap("__",'map')
+                text += maze.createStruct("__",'map',x,i);
             }else if(value[i]==1){
-                maze.printMap('##','map')
+                text += maze.createStruct('##','map',x,i);
             }else if(value[i]==2){
-                maze.printMap('&#127852;','map')
+                text += maze.createStruct('&#127852;','map',x,i);
             }
         }
-        //maze.printMap(']','map')
-        maze.printMap('<br>','map')
+
+        text += maze.createStruct('','map',x,'end');
+        maze.printMap(text,'map');
     })
-}
+};
 
 
-maze.printMap = function (zeichen, className) {
-    jQuery("." + className).append(zeichen);
-}
+maze.createStruct = function (zeichen, className, x, y) {
+    var env;
+    if(y==="start"){
+        env = '<div id="'+x+'">'+zeichen;
+    }else if(y==="end"){
+        env = zeichen+'</div>';
+    }else{
+         env = '<span id="'+ x +'.'+y+'">'+zeichen+'</span>';
+    }
+return env;
+};
+
+
+maze.printMap = function (env, divName) {
+    jQuery("." + divName).append(env);
+};
