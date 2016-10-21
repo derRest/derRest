c7d6cfaff40a54dcc4e91939ed52108039428425
Jan Jäger - PC2520 <privat@jan-jaeger.de>
2016-10-21 14:41:24 +0200

    ADD startGame()

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -10,18 +10,23 @@ game.config = {
     playerSymbol: "&#916;"
 };
 
+game.startTime = 0;
+
 game.player.setPos = function (x, y) {
     var locationId = x + maze.config.splitChar + y;
-    //if(maze.validatePostition()===true){          //Prüft ob nicht auf Mauer gelaufen wird
-        //console.log(locationId);
-    
+    if(maze.isWall(x,y)===true){                //Prüft ob nicht auf Mauer gelaufen wird
+        console.log("isWall: "+maze.isWall(x,y));
+
         game.player.setPlayer(x,y);
         jQuery("#"+game.player.getPos()).removeAttr(game.config.player,"");     //Altes Attribut für Position löschen
         jQuery("#"+locationId).attr(game.config.player,"test");                 //Neues Attribut für Position setzen
 
-    //}else{console.log("Unerlaubter Zug!";}
-};
+        //TODO maze.candyInPosition()
 
+    }else{
+        console.log("Unerlaubter Zug!");
+    }
+};
 
 game.player.getPos = function () {
     return jQuery("span[player]").attr("id");
@@ -37,3 +42,23 @@ game.player.setPlayer = function (x,y) {
     jQuery("#"+locationId).toggleClass("player");                           //setze Klasse "player" für die Farbe
 
 };
+
+game.startGame = function () {
+    loadJson();
+    game.timeMeassure("start");
+};
+
+game.timeMeassure = function (start) {
+    var jetzt = new Date();
+    if(start=="start"){
+        game.startTime = Date.now();
+        console.log("Zeit Messung gestartet: "+ game.startTime);
+    }else{
+        var TimeDiff = 0;
+       return TimeDiff = Date.now() - game.startTime;
+        console.log("Zeit Messung beendet Diff: "+ TimeDiff);
+    }
+};
+
+
+
