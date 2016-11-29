0410fc09b770236180eafaf14155ccc24656f472
Florian <schuessler-f@online.de>
2016-11-29 11:40:39 +0100

    added 3 tests for candy Check

--- tests/MazeTest.php
+++ tests/MazeTest.php
@@ -32,17 +32,57 @@ class MazeTest extends \PHPUnit_Framework_TestCase
 
     public function testCandyCount1()
     {
+        //Test for correct amount of candies
+        $maze = new Maze(15,15,5);
+        $maze = $maze->generate()->getMaze();
 
+        $candyCount = 0;
+        foreach ($maze as $mazeLine) {
+            foreach ($mazeLine as $cell) {
+                if ($cell === Maze::CANDY) {
+                    $candyCount++;
+                }
+            }
+        }
+
+        $this->assertEquals(5, $candyCount);
     }
 
     public function testCandyCount2()
     {
+        //more then max candies
+        $maze = new Maze(15,15,5000);
+        $maze = $maze->generate()->getMaze();
 
+        $candyCount = 0;
+        foreach ($maze as $mazeLine) {
+            foreach ($mazeLine as $cell) {
+                if ($cell === Maze::CANDY) {
+                    $candyCount++;
+                }
+            }
+        }
+
+        $whitespaceCount = 0;
+        foreach ($maze as $mazeLine) {
+            foreach ($mazeLine as $cell) {
+                if ($cell === Maze::WHITE_SPACE) {
+                    $whitespaceCount++;
+                }
+            }
+        }
+
+        $this->assertEquals(0, $whitespaceCount);
+        $this->assertGreaterThanOrEqual(1, $candyCount);
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
     public function testCandyCount3()
     {
-
+        //negative number of candies
+        $maze = new Maze(15,15,-5);
     }
 
     public function testMazeChanging()
